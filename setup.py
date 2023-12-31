76281e380b3e54fd8be7d15c3ad1f0862d4f2886
hitendra0165 <hitendrabhamare007@gmail.com>
2023-12-31 12:50:04 +0530

    app is ready

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup (
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Hitendra',
+    author_email = 'hitendrabhamare007@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
